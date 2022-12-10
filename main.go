57287472bff2e8eacd8d86579326bb379655b2ba
何中峰 <1446450047@qq.com>
2022-12-10 23:50:22 +0800

    添加文件接口

--- main.go
+++ main.go
@@ -93,6 +93,32 @@ func QrcodesController(c *gin.Context) {
 		c.Status(http.StatusBadRequest)
 	}
 }
+func FilesController(c *gin.Context) {
+	file, err := c.FormFile("raw")
+	if err != nil {
+		log.Fatal(err)
+	}
+	exe, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dir := filepath.Dir(exe)
+	if err != nil {
+		log.Fatal(err)
+	}
+	filename := uuid.New().String()
+	uploads := filepath.Join(dir, "uploads")
+	err = os.MkdirAll(uploads, os.ModePerm)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fullpath := path.Join("uploads", filename+filepath.Ext(file.Filename))
+	fileErr := c.SaveUploadedFile(file, filepath.Join(dir, fullpath))
+	if fileErr != nil {
+		log.Fatal(fileErr)
+	}
+	c.JSON(http.StatusOK, gin.H{"url": "/" + fullpath})
+}
 func main() {
 
 	go func() {
@@ -102,6 +128,7 @@ func main() {
 		router.GET("/uploads/:path", UploadsController)
 		router.GET("/api/v1/qrcodes", QrcodesController)
 		router.POST("/api/v1/texts", TextsController)
+		router.POST("/api/v1/files", FilesController)
 		router.GET("/api/v1/addresses", AddressesController)
 		router.StaticFS("/static", http.FS(staticFiles))
 		router.NoRoute(func(c *gin.Context) {
